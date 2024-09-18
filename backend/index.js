dbb6697e993f4bea4de42ff305c59c13e1616fda
BALAJI934428 <142514847+BALAJI934428@users.noreply.github.com>
2024-09-18 15:22:18 +0530

    commit test

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ mongoose.connection.on("connected", () => {
   console.log("mongoDB connected");
 });
 
-//middlewares
+//middleware
 app.use("/auth", authRoute);
 app.listen(8800, () => {
   connect();
